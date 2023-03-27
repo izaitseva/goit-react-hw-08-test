a65312c24c1af235cd1887f8a551bb157f29bfe9
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-27 23:42:55 +0300

    conditions create contacts

--- src/features/contacts/CreateContacts.js
+++ src/features/contacts/CreateContacts.js
@@ -21,47 +21,48 @@ const CreateContacts = () => {
     }
 
     const onAddContact = () => {
-        if (newContactName.trim() !== '' && newContactNumb.trim() !== '') {
-            if (contacts.some(el => el.name.toLowerCase() === newContactName.toLowerCase())) {
-                alert(`${newContactName} is already in contacts`)
-            } else {
-                const newContact = { name: newContactName, number: newContactNumb }
-                dispatch(addContact(newContact));
-                setNewContactName('');
-                setNewContactNumb('');
-            }
+        if (newContactName.trim() === '' || newContactNumb.trim() === '') {
+            return alert(`Please fill all the fields`)
         }
-    }
+       else if (contacts.some(el => el.name.toLowerCase() === newContactName.toLowerCase())) {
+            alert(`${newContactName} is already in contacts`)
+        } else {
+            const newContact = { name: newContactName, number: newContactNumb }
+            dispatch(addContact(newContact));
+            setNewContactName('');
+            setNewContactNumb('');
+        }
+}
 
-    return (
-        <div>
-            <div className={styles.input_group}>
-                <input className={styles.input}
-                    placeholder="Enter the contact name"
-                    type="text"
-                    name="name"
-                    value={newContactName}
-                    onChange={handleContactName}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                />
-                <input className={styles.input}
-                    placeholder="Enter the contact phone"
-                    type="tel"
-                    name="number"
-                    value={newContactNumb}
-                    onChange={handleContactNumb}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                />
-                <button className={styles.add_btn} onClick={onAddContact} type="submit">
-                    <FaUserPlus />
-                </button>
-            </div>
-        </div >
-    )
+return (
+    <div>
+        <div className={styles.input_group}>
+            <input className={styles.input}
+                placeholder="Enter the contact name"
+                type="text"
+                name="name"
+                value={newContactName}
+                onChange={handleContactName}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+            />
+            <input className={styles.input}
+                placeholder="Enter the contact phone"
+                type="tel"
+                name="number"
+                value={newContactNumb}
+                onChange={handleContactNumb}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+            />
+            <button className={styles.add_btn} onClick={onAddContact} type="submit">
+                <FaUserPlus />
+            </button>
+        </div>
+    </div >
+)
 }
 
 export default CreateContacts;
