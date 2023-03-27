e763d6f19f51f6a7f71af668ef12bd05428d573e
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-27 23:47:25 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-homework-template">
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <Provider store={store}>
         <App />
       </Provider>
